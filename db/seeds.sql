781772a8f56887a95cc6d71957a6513a5537a3ba
Michael Mansi <mmansi18@gmail.com>
2018-05-05 10:06:04 -0700

    finishing touches

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Mushroom Swiss', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Bacon Western', false);
+
